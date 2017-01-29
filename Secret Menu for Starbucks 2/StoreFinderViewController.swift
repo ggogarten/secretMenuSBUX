5caabee7d1d826ffa6b78bdaec94c0ad4aa0cbd4
George Gogarten <georgegogarten@Georges-MacBook-Pro.local>
2017-01-29 08:43:32 -0500

    initial commit, just basic storyboards so far

---
+++ Secret Menu for Starbucks 2/StoreFinderViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StoreFinderViewController.swift
+//  Secret Menu for Starbucks 2
+//
+//  Created by George Gogarten on 1/29/17.
+//  Copyright © 2017 George Gogarten. All rights reserved.
+//
+
+import UIKit
+
+class StoreFinderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Secret Menu for Starbucks 2/ViewController.swift
+++ Secret Menu for Starbucks 2/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    
+//    context function for swift 3.0
+    func getContext () -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+     
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
